8df92f59f2d2c6344d2db00e5c2e1275c0f0ca99
antoniosumak <sumakantonio@gmail.com>
2021-07-13 15:46:28 +0200

    Added routes for pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+import Store from './pages/Store';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Route exact path="/" component={Store} />
+      <Route path="/login" component={Login} />
+      <Route path="/cart" component={Cart} />
+    </>
+  );
 }
 
 export default App;
