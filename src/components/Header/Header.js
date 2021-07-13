2d92b25981e41631e758d2c290fab3c1eee3205c
antoniosumak <sumakantonio@gmail.com>
2021-07-13 20:15:11 +0200

    Header styling almost done, hamburger menu problems

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import { HeaderWrapper } from './HeaderStyles';
+import {
+  HeaderWrapper,
+  HeaderInner,
+  LogoContainer,
+  NavItems,
+  Logo,
+  Nav,
+  InnerNav,
+  ShoppingCart,
+  Hamburger,
+  HamburgerLine,
+} from './HeaderStyles';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
-  return <HeaderWrapper></HeaderWrapper>;
+  const links = [
+    { label: 'Shop', path: '/shop' },
+    { label: 'Login', path: '/login' },
+    { label: <ShoppingCart />, path: '/cart' },
+  ];
+
+  return (
+    <HeaderWrapper>
+      <HeaderInner>
+        <LogoContainer>
+          <Logo src={logo} />
+        </LogoContainer>
+        <Nav>
+          <InnerNav>
+            {links.map((value, index) => (
+              <NavItems key={index} to={value.path}>
+                {value.label}
+              </NavItems>
+            ))}
+          </InnerNav>
+        </Nav>
+        <Hamburger>
+          <HamburgerLine />
+          <HamburgerLine />
+          <HamburgerLine />
+        </Hamburger>
+      </HeaderInner>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,18 +1,95 @@
 import styled from 'styled-components';
-import { breakpoints } from '../../lib/styles/theme';
+import { boxShadow, breakpoints, colors } from '../../lib/styles/theme';
+import { FaShoppingCart } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 
 export const HeaderWrapper = styled.header`
-  padding: 40px 15px;
-  width: 100%;
+  height: 72px;
+  box-shadow: ${boxShadow};
+
   @media screen and (${breakpoints.tablet}) {
-    padding: 64px 40px;
+    height: 96px;
+  }
+`;
+
+export const HeaderInner = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (${breakpoints.tablet}) {
+    padding: 0 40px;
   }
   @media screen and (${breakpoints.desktop}) {
-    padding: 80px 0;
     max-width: 944px;
     margin: 0 auto;
   }
   @media screen and (${breakpoints.desktopLarge}) {
     max-width: 1224px;
   }
+`;
+
+export const LogoContainer = styled.figure`
+  width: 100px;
+  height: 40px;
+`;
+
+export const Logo = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`;
+
+export const Nav = styled.nav`
+  display: none;
+
+  @media screen and (${breakpoints.tablet}) {
+    display: block;
+  }
+`;
+
+export const InnerNav = styled.div`
+  display: flex;
+`;
+
+export const NavItems = styled(NavLink)`
+  text-decoration: none;
+  color: ${colors.black};
+  padding: 0px 18px;
+  font-size: 16px;
+  font-weight: bold;
+
+  &:last-child {
+    padding-right: 0px;
+  }
+
+  &:visited {
+    color: ${colors.black};
+  }
+`;
+
+export const ShoppingCart = styled(FaShoppingCart)`
+  font-size: 24px;
+  color: ${colors.red};
+`;
+
+export const Hamburger = styled.div`
+  width: 24px;
+  height: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (${breakpoints.tablet}) {
+    display: none;
+  }
+`;
+
+export const HamburgerLine = styled.div`
+  width: 100%;
+  height: 4px;
+  background-color: ${colors.black};
+  border-radius: 10px;
 `;
